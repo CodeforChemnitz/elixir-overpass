5db7541b656dba7dd7d9a02b6a13c02700a48bd0
Tobias Gall <tobias.gall@s2010.tu-chemnitz.de>
2015-11-06 18:43:07 +0100

    erste structs

--- lib/overpass.ex
+++ lib/overpass.ex
@@ -1,2 +1,37 @@
 defmodule Overpass do
+
+    defmodule Tag do
+        @doc """
+        http://wiki.openstreetmap.org/wiki/Tags
+        """
+        defstruct k: "", v: ""
+    end
+
+    defmodule Node do
+        @doc """
+        http://wiki.openstreetmap.org/wiki/Node
+        """
+        defstruct id: "", lat: "", lon: "", attributes: %{}, tags: []
+    end
+
+    defmodule Way do
+        @doc """
+        http://wiki.openstreetmap.org/wiki/Way
+        """
+        defstruct nds: [], tags: []
+    end
+
+    defmodule Relation do
+        @doc """
+        http://wiki.openstreetmap.org/wiki/Relation
+        """
+        defstruct members: [], tags: []
+    end
+
+    defmodule RelationMember do
+        @doc """
+        http://wiki.openstreetmap.org/wiki/Relation
+        """
+        defstruct typ: "", ref: "", role: ""
+    end
 end
--- test/overpass_api_test.exs
+++ test/overpass_api_test.exs
@@ -7,15 +7,16 @@ defmodule OverpassAPITest do
       assert {:error, _} = result
   end
 
-  test "Overpass.API with JSON" do
+  test "Overpass.API with QL and JSON-Response" do
       {:ok, {:json, result}} = Overpass.API.query("[out:json];node[\"name\"=\"Gielgen\"];out 2;")
       {:ok, %HTTPoison.Response{status_code: _code, body: body, headers: _headers}} = HTTPoison.get("http://overpass.osm.rambler.ru/cgi/interpreter?data=%5Bout:json%5D;node%5Bname%3DGielgen%5D%3Bout%202%3B")
       assert result == body
   end
 
-  test "Overpass.API with XML" do
+  test "Overpass.API with QL and XML-Response" do
       {:ok, {:xml, result}} = Overpass.API.query("[out:xml];node[\"name\"=\"Gielgen\"];out 2;")
       {:ok, %HTTPoison.Response{status_code: _code, body: body, headers: _headers}} = HTTPoison.get("http://overpass.osm.rambler.ru/cgi/interpreter?data=node%5Bname%3DGielgen%5D%3Bout%202%3B")
       assert result == body
   end
+
 end
