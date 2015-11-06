152ec8261de204f48ef01344fb195ae334bd7060
tobias <toga@tu-chemnitz.eu>
2015-11-06 01:29:04 +0100

    api in extra file & test gegen die beispiele

---
+++ lib/overpass_api.ex
@@ -0,0 +1,51 @@
+defmodule Overpass.API do
+
+    require Logger
+
+    @url Application.get_env(:overpass, :url)
+
+    def query(query, format \\ "json")
+    def query(query, format) do
+        query = "[out:" <> format <> "];" <> query
+        Logger.debug("Query: #{query}")
+        HTTPoison.post(@url, query)
+        |> process_response
+        |> process_body
+    end
+
+    defp process_response({:ok, %HTTPoison.Response{status_code: 200, body: body, headers: headers}}) do
+        Logger.debug("status_code: 200")
+        %{"Content-Type" => content_type} = Enum.into(headers, %{})
+        {:ok, content_type, body}
+    end
+
+    defp process_response({:ok, %HTTPoison.Response{status_code: code, body: _body, headers: _headers}}) do
+        Logger.error("status_code: #{code}")
+        {:error, "status_code: #{code}"}
+    end
+
+    defp process_response({:error, error}) do
+        Logger.error(error)
+        {:error, error}
+    end
+
+    defp process_body({:ok, content_type, body}) when content_type == "application/osm3s+xml" do
+        Logger.debug("application/osm3s+xml")
+        {:ok, body}
+    end
+
+    defp process_body({:ok, content_type, body}) when content_type == "application/json" do
+        Logger.debug("application/json")
+        {:ok, body}
+    end
+
+    defp process_body({:ok, _content_type, _body}) do
+        Logger.error("Unsuported Content-Type")
+        {:error, "Unsuported Content-Type"}
+    end
+
+    defp process_body({:error, error}) do
+        Logger.error(error)
+        {:error, error}
+    end
+end
--- test/overpass_test.exs
+++ test/overpass_test.exs
@@ -1,34 +1,23 @@
 defmodule OverpassTest do
   use ExUnit.Case
   require Logger
-  import SweetXml
   doctest Overpass
 
+  test "Overpass 400er" do
+      result = Overpass.API.query("foobar")
+      assert {:error, _} = result
+  end
+
   test "Overpass JSON" do
-      result = Overpass.API.query("node[\"name\"=\"Gielgen\"];out body;", "json")
-      assert :jsx.is_json(result)
+      {:ok, result} = Overpass.API.query("node[\"name\"=\"Gielgen\"];out 2;", "json")
+      {:ok, %HTTPoison.Response{status_code: _code, body: body, headers: _headers}} = HTTPoison.get("http://overpass.osm.rambler.ru/cgi/interpreter?data=%5Bout:json%5D;node%5Bname%3DGielgen%5D%3Bout%202%3B")
+      assert result == body
   end
 
   test "Overpass XML" do
-      result = Overpass.API.query("node[\"name\"=\"Gielgen\"];out body;", "xml")
-      assert is_binary(result)
-  end
-
-  test "get ways as xml" do
-    result = Overpass.API.query """
-    way(50.746,7.154,50.748,7.157) ["highway"];
-    (._;>;);
-    out body;
-    """, "xml"
-    assert {:ok, _} = result
-
-    {:ok, %{ways: ways, nodes: nodes}} = result
-    #OverpassHelper.get_attr_list ways, "lat"
-    latlongs = nodes
-    |> Stream.map(fn el -> { xpath(el, ~x"./@lat"s), xpath(el, ~x"./@lon"s) } end)
-    |> Enum.into []
-    assert is_list(latlongs)
-    Logger.debug fn -> inspect(latlongs) end
+      {:ok, result} = Overpass.API.query("node[\"name\"=\"Gielgen\"];out 2;", "xml")
+      {:ok, %HTTPoison.Response{status_code: _code, body: body, headers: _headers}} = HTTPoison.get("http://overpass.osm.rambler.ru/cgi/interpreter?data=node%5Bname%3DGielgen%5D%3Bout%202%3B")
+      assert result == body
   end
 
 end
